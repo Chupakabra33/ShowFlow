815e35f18f512b70b25abc1bf774342830dcef13
Keti Georgieva <51882310+Chupakabra33@users.noreply.github.com>
2020-02-25 07:10:49 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+		<!-- footer.php -->
+		<footer class="footer">
+			<div class="footer">Designed and powered by Keti Georgieva</div>
+		</footer>
+	</div>
+
+
+	<script src="./script/script_header.js?v1.1.6"></script>
+	<script src="./script/script_events.js?v1.0.1"></script>
+	<script src="./script/script_contacts.js?v1.0.2"></script>
+</body>
+</html>
