ad76b5affa5d16e1ed3d012966ea0c11f6b719e4
Keti Georgieva <51882310+Chupakabra33@users.noreply.github.com>
2020-02-29 20:56:41 +0200

    Update database_connection.inc.php

--- includes/database_connection.inc.php
+++ includes/database_connection.inc.php
@@ -2,8 +2,8 @@
 //database_connection.inc.php
 
 $servername = 'localhost';
-$db_username = 'Admin';
-$db_password = 'Chupakabra33';
+$db_username = 'root';
+$db_password = '';
 $db_name = 'show_flow';
 $con = mysqli_connect($servername, $db_username, $db_password, $db_name);
 
